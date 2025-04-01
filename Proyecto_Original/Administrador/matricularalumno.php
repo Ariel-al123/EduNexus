976de6b77de3147d1e97e32875bbb3dd54e8a414
Ariel-al123 <abetancourt@ucol.mx>
2025-04-01 14:14:02 -0600

    Add Initial Project

---
+++ Proyecto_Original/Administrador/matricularalumno.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Matricular un alumno</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+</head>
+<body>
+    <h1>Matricular un alumno</h1> 
+    <!-- Formulario -->
+    <form action="" method="post">
+        <h2>Alumnos:</h2>
+        <select name="alumno" id="alumnoSelect">
+            <p>Selecciona un alumno: <p>
+            <?php
+            require_once("contacto.php");
+            $obj = new Contacto();
+            $resultado = $obj->consultaralumno();
+            while($registro = $resultado->fetch_assoc()){
+                echo "<option value=".$registro["id"].">".$registro["nombre"]." ".$registro["apellido_paterno"]." ".$registro["apellido_materno"]."</option>";
+            }
+            ?>
+        </select>
+
+        <h2>Asignatura:</h2>
+        <select name="asignatura" id="asignaturaSelect">
+            <?php
+            $resultado = $obj->consultarasignatura();
+            while($registro = $resultado->fetch_assoc()){
+                echo "<option value=".$registro["id"].">".$registro["nombre"]."</option>";
+            }
+            ?>
+        </select>
+
+        <input type="submit" name="insert" value="Guardar" class="boton">
+    </form>
+</body>
+</html>
+
+<?php
+// Incluir el archivo PHP que contiene la clase Contacto
+require_once("contacto.php");
+
+// Si se ha enviado el formulario
+if(isset($_POST['insert'])){
+    // Recuperar los valores del formulario
+    $idalumno = isset($_POST['alumno']) ? $_POST['alumno'] : '';
+    $idasignatura = isset($_POST['asignatura']) ? $_POST['asignatura'] : '';
+    
+    // Verificar si el alumno ya está matriculado en la asignatura seleccionada
+    $obj = new contacto();
+    $resultado = $obj->consultarsolounalumnomatriculado($idalumno, $idasignatura);
+    
+    if($resultado->num_rows > 0) {
+        // Mostrar un mensaje indicando que el alumno ya está matriculado en esta asignatura
+        echo "El alumno ya está matriculado en esta asignatura.";
+    } else {
+        // Obtener detalles del alumno y la asignatura para la matrícula
+        $obj = new contacto();
+        $resultado = $obj->cargaralumno($idalumno);
+        while ($registro = $resultado->fetch_assoc()) {
+            $nombrealumno = $registro["nombre"];
+            $apellidopaterno = $registro["apellido_paterno"];
+            $apellidomaterno = $registro["apellido_materno"];
+        }
+
+        $obj2 = new contacto();
+        $resultado = $obj2->cargarasignatura($idasignatura);
+        while ($registro = $resultado->fetch_assoc()) {
+            $nombreasignatura = $registro["nombre"];
+        }
+
+        // Matricular al alumno si no está matriculado en la asignatura
+        $obj3 = new contacto();
+        $obj3->matricularalumno($idasignatura,$nombreasignatura,$idalumno,$nombrealumno,$apellidopaterno,$apellidomaterno);
+
+        echo "Alumno Matriculado";
+    }
+}
+?>
